3d2222bdcb6688282e83aef9811859d208672ef7
Azrieell <azriellrsiddiq@gmail.com>
2023-10-31 15:14:28 +0700

    Edit

--- src/store/modules/admin/user.js
+++ src/store/modules/admin/user.js
@@ -3,17 +3,25 @@ import axios from "axios";
 const user = {
     namespaced: true,
     state: {
-        user: []
+        user: [],
+        singleId: [],
+        errorUpdated: null
     },
     getters: {
         getuser: (state) => state.user,
+        getError: (state) => state.errorUpdated,
         getUserById: (state) => (uuid) => {
             console.log("fetching single User by id:", uuid);
             console.log("ProductData:", state.user);
             const user = state.user;
             console.log("user:", user);
             return user;
-        }
+        },
+        getSingleById: (state) => (useruuid) => {
+            const getSingleUser = state.user.find((p) => p.uuid == useruuid);
+            console.log("User:", getSingleUser);
+            return getSingleUser;
+        },
     },
     actions: {
         async fetchUser({
@@ -22,6 +30,18 @@ const user = {
             try {
                 const response = await axios.get('http://localhost:5000/api/v1/users');
                 commit('SET_USER', response.data)
+                return response.data
+            } catch (error) {
+                alert(error.message)
+                return false
+            }
+        },
+        async fetchUserById({
+            commit
+        }, useruuid) {
+            try {
+                const response = await axios.get(`http://localhost:5000/api/v1/users/${useruuid}`);
+                commit('SET_SINGLE_USER', response.data)
                 return response.data
             } catch (error) {
                 alert(error.message)
@@ -52,14 +72,17 @@ const user = {
         async updateUser({
             commit
         }, {
-            uuid,
-            updatedData
+            useruuid,
+            userData
         }) {
             try {
-                const response = await axios.patch(`http://localhost:5000/api/v1/users/update/${uuid}`, updatedData);
-                commit('updateUser', response.data);
+                const response = await axios.patch(`http://localhost:5000/api/v1/users/update/${useruuid}`, userData);
+                commit('updateUserData', response.data);
+                return response.data
             } catch (error) {
-                console.error('Error updating user', error);
+                const updateError = error.response.data.message;
+                commit('ERROR_UPDATE_USER', updateError)
+                throw error
             }
         },
         deleteUser({
@@ -75,22 +98,26 @@ const user = {
                     console.error('Error deleting position:', error);
                 });
         },
-        
     },
     mutations: {
+        ERROR_UPDATE_USER(state, error) {
+            state.errorUpdated = error
+        },
         SET_USER(state, user) {
             state.user = user
         },
         SET_ADD_USER(state, user) {
             state.user = user
         },
-        updateUser(state, updatedUser) {
-            const index = state.user.findIndex((user) => user.uuid === updatedUser.uuid);
-            if (index !== -1) {
-                state.users.splice(index, 1, updatedUser);
-            }
+        SET_SINGLE_USER(state, user) {
+            state.singleId = user
         },
+        updateUserData(state, updatedUser) {
+            const index = state.user.findIndex((userItem) => userItem.uuid === updatedUser.uuid);
+            if (index !== -1) {
+                Object.assign(state.user[index], updatedUser);
+            }
+        }
     }
 }
-
 export default user;
