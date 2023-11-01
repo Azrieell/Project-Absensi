133bd4ccedde91d7014e438e4188505cf92fcff5
agengpry <yogaazee@gmail.com>
2023-11-01 09:57:25 +0700

    page karyawan

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   
   <!-- <SidebarComponentAdmin/> -->
-  <NavbarComponent></NavbarComponent>
-  <FooterComponent></FooterComponent>
+  <NavbarComponent></NavbarComponent> 
+  <DropdownComponent></DropdownComponent>
   <router-view></router-view>
+  <FooterComponent></FooterComponent>
 </template>
 
 <script setup>
   // import SidebarComponentAdmin from './components/admin/SidebarComponentAdmin.vue'
   import NavbarComponent from './components/karyawan/NavbarComponent.vue'
   import FooterComponent from './components/karyawan/FooterComponent.vue'
+  import DropdownComponent from './components/karyawan/DropdownComponent.vue'
+  import "flowbite/dist/flowbite"
 </script>
-
