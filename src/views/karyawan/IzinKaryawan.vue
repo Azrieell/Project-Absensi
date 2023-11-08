e0a83c3c02f029428da8ac1ecf7b4b9fe5d74502
Azrieell <azriellrsiddiq@gmail.com>
2023-11-08 10:33:30 +0700

    Karyawan

--- src/views/karyawan/IzinKaryawan.vue
+++ src/views/karyawan/IzinKaryawan.vue
@@ -5,11 +5,11 @@
             <h5 class="text-xl text-center font-bold leading-none text-gray-900 dark:text-white">Izin</h5><br>
         </div>
 
-        <div class="w-full max-w-sm  rounded-lg" style="margin-left: 30%;">
+        <div class="w-full max-w-sm mx-auto rounded-lg">
 
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
             <textarea id="message" rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-black-500 focus:border-black-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-black-500 dark:focus:border-black-500"
+                class="block p-2.5 sm:w-full w-60 sm:-ml-0 -ml-24 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-black-500 focus:border-black-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-black-500 dark:focus:border-black-500"
                 placeholder="Keterangan tidak hadir"></textarea>
         </div>
         <button type="button" class="text-white mt-5 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
--- src/views/karyawan/SakitKaryawan.vue
+++ src/views/karyawan/SakitKaryawan.vue
@@ -5,7 +5,8 @@
             <h5 class="text-xl text-center font-bold leading-none text-gray-900 dark:text-white">Sakit</h5><br>
         </div>
 
-        <div class="w-full max-w-sm  rounded-lg" style="margin-left: 35%;">
+        
+        <div class="w-full max-w-sm mx-auto rounded-lg">
 
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
             <textarea id="message" rows="4"
@@ -17,12 +18,15 @@
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload
                 file</label>
             <input
-                class="block max-w text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                id="file_input" type="file" style="width: 380px;">
+                class="block max-w-sm sm:mx-auto -ml-10 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                id="file_input" type="file">
         </div>
-
-        <button type="button" class="text-white mt-5 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-            style="margin-left: 50%; background-color: darkgray;">Kirim</button>
+        <div class="flex justify-center max-w-sm mx-auto">
+        <button type="button" class="text-white mt-5 font-medium rounded-lg text-sm px-6 py-2.5 justify-center mb-2 "
+            style="background-color: darkgray;">Kirim
+        </button>
+        </div>
+            
 
     </div>
 </template>
